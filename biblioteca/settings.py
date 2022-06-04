6abab470fe9a0a08a7da692d39693342c6b24029
leocassiosilva <leocassiosilva1234@gmail.com>
2022-06-04 16:34:57 -0300

    staticfile

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -129,3 +129,18 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
